d95d60c4965b405e770ee54d1e44cdc4affacdc2
Meerim Maratova <maratovameerim@gmail.com>
2024-10-10 11:49:47 +0600

    used code formatter/ DONE

--- jokes-app/src/App.tsx
+++ jokes-app/src/App.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
-import Joke from './Components/Joke/Joke.tsx';
-
+import { useEffect, useState } from "react";
+import Joke from "./Components/Joke/Joke.tsx";
 
 function App() {
-
-  const [joke, setJoke] = useState<string>('');
+  const [joke, setJoke] = useState<string>("");
 
   const getJokes = async () => {
     try {
       const response = await fetch("https://api.chucknorris.io/jokes/random");
       const jokes = await response.json();
       setJoke(jokes.value);
-    } catch(error) {
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     getJokes();
-  }, [])
+  }, []);
 
   return (
     <>
       <div className="container p-2">
         <Joke joke={joke} />
-        <button type="button" onClick={getJokes} className="btn btn-primary">New Joke</button>
+        <button type="button" onClick={getJokes} className="btn btn-primary">
+          New Joke
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
