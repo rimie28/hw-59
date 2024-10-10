3a65b605e184d4c20c3b73a9e350d712b1215f64
Meerim Maratova <maratovameerim@gmail.com>
2024-10-10 11:48:23 +0600

    made API request and added a button to create New Joke

--- jokes-app/src/App.tsx
+++ jokes-app/src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
+import Joke from './Components/Joke/Joke.tsx';
 
 
 function App() {
 
+  const [joke, setJoke] = useState<string>('');
+
+  const getJokes = async () => {
+    try {
+      const response = await fetch("https://api.chucknorris.io/jokes/random");
+      const jokes = await response.json();
+      setJoke(jokes.value);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getJokes();
+  }, [])
 
   return (
     <>
-
+      <div className="container p-2">
+        <Joke joke={joke} />
+        <button type="button" onClick={getJokes} className="btn btn-primary">New Joke</button>
+      </div>
     </>
   )
 }
---
+++ jokes-app/src/Components/Joke/Joke.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface JokeProps {
+  joke: string;
+}
+
+const Joke:React.FC<JokeProps> = ({joke}) => {
+  return (
+    <div className="mt-2 mb-3">
+      <h4>{joke}</h4>
+    </div>
+  )
+}
+
+export default Joke;
